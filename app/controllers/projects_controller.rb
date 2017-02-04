6e9f8b6dd39c96468b6a2127efdf895f743d006d
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-02-04 17:12:23 +0000

    added form to add new project and added font-awesome

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,19 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :note, :label, :priority)
+  end
 end
