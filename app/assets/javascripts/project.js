a252cc57a27f1ddb0c9abd1fd1e9a1dbc3711936
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-02-05 03:33:45 +0000

    starting styling the simple form

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,3 +1,7 @@
 $(document).on('ready page:load', function () {
-  $("h1").css("color", "red");
+  $("form").find("div").addClass("simple-form-children");
+  $(".simple-form-children").children().addClass("form-items");
+  $("input").last().css("width","50px");
+
+
 });
