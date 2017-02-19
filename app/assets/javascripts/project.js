924a648a078cd2946614d4315ac7e25780b2b24b
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-02-19 20:54:57 +0000

    added icons

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -2,5 +2,25 @@
 $(document).on('turbolinks:load', function() {
   $('.new_project').find('label').css("margin-right", "30px");
      $('.modal').modal();
+     var artwork = $('.collection_radio_buttons').eq(0);
+     var arch = $('.collection_radio_buttons').eq(1);
+     var furniture = $('.collection_radio_buttons').eq(2);
+     var craft = $('.collection_radio_buttons').eq(3);
+     var app = $('.collection_radio_buttons').eq(4);
+
+     artwork.addClass('art-image');
+     arch.addClass('arch-image');
+     furniture.addClass('furn-image');
+     craft.addClass('craft-image');
+     app.addClass('app-image');
+
+    $('.collection_radio_buttons').click(function(){
+      $('.collection_radio_buttons').removeClass('selected-outline');
+      $(this).addClass('selected-outline');
+
+    });
+
+
+
 
 });
